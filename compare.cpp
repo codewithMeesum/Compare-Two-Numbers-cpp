3705c34e796117a097b0d188bd3d6e3ae73a345c
Meesum <mesummukhtar3@gmail.com>
2025-07-10 22:34:06 +0500

    Add files via upload

---
+++ compare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int x,y;
+	cout<<"enter value of x=";
+	cin>>x;
+	cout<<"enter value of y =";
+	cin>>y;
+	if (x>y){
+		cout<<"x is greater";
+	}
+	else {
+	cout<<"y is greater ";
+}
+}
